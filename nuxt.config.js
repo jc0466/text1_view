2f39568d377becafe594a1c5948c0d780d8f2442
limjuchan <sa0466@nate.com>
2018-05-29 11:53:21 +0900

    2018-05-29_5

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,51 +1,50 @@
-module.exports = { 
-     /* 
-     ** Headers of the page 
-     */ 
-     head: { 
-      title: "개사랑", 
-       meta: [ 
-         { charset: "utf-8" }, 
-         { name: "viewport", content: "width=device-width, initial-scale=1" }, 
-         { hid: "description", name: "description", content: "대한민국 애견인의 개사랑" } 
-       ], 
-       link: [ 
-         { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }, 
-         { 
-           rel: "stylesheet", 
-           href: 
-             "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css" 
-         } 
-       ], 
-       script: [ 
-         { 
-           src: "https://use.fontawesome.com/releases/v5.0.7/js/all.js", 
-           defer: "" 
-         } 
-       ] 
-     }, 
-     /* 
-    ** Customize the progress bar color 
-     */ 
-     loading: { color: "#3B8070" }, 
-     /* 
-     ** Build configuration 
-     */ 
-     build: { 
-       vender: 'axios',
-       /* 
-       ** Run ESLint on save 
-       */ 
-       extend(config, { isDev, isClient }) { 
-         if (isDev && isClient) { 
-           config.module.rules.push({ 
-             enforce: "pre", 
-             test: /\.(js|vue)$/, 
-             loader: "eslint-loader", 
-            exclude: /(node_modules)/ 
-           }); 
-        } 
-       } 
-     } 
-   }; 
-  
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: "개사랑",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "대한민국 애견인의 개사랑" }
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css"
+      }
+    ],
+    script: [
+      {
+        src: "https://use.fontawesome.com/releases/v5.0.7/js/all.js",
+        defer: ""
+      }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: "#3B8070" },
+  /*
+  ** Build configuration
+  */
+  build: {
+    vendor: ['axios'],
+    /*
+    ** Run ESLint on save
+    */
+    extend(config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/
+        });
+      }
+    }
+  }
+};
--- pages/dogs/_dog.vue
+++ pages/dogs/_dog.vue
@@ -1,31 +1,28 @@
 <template>
- <section class="section">
-     <article class="hero"> 
-         <div class="hero-body">
-             <div class="container">
-                 <h1 class="title">저는 {{dogName.toUpperCase()}}입니다.</h1>
-                 <h2 class="subtitle">많이 사랑해주세요!</h2>
-         </div>
-         </div>
-     </article>
-     <article class="notification">
-       <figure class="image is-square">
-          <img :src="image" alt="">
-       </figure>
-     </article>
- </section>
+    <section class="section">
+        <article class="hero is-info">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">저는 {{dogName.toUpperCase()}}입니다.</h1>
+                    <h2 class="subtitle">많이 사랑해주세요!</h2>
+                </div>
+            </div>
+        </article>
+        <article class="notification">
+            <figure class="image is-square">
+                <img :src="image" alt="">
+            </figure>
+        </article>
+    </section>
 </template>
 <script>
-  import axios from 'axios';
-  
-   export default {
-     asyncData({params}) {
-         const image = await axios.get();
-
-       return {
-           dogname: params.dog,
-           image: image.data.message
-       };
-     }
-     };
-</script>
+    import axios from 'axios';
+    export default {
+        async asyncData({ params }) {
+            const image = await axios.get(`https://dog.ceo/api/breed/${params.dog}/images/random`);
+            return {
+                dogName: params.dog,
+                image: image.data.message
+            };
+        }
+    };
