21bc28004caa2bc0e4b783ca4a59b3bf963e3824
limjuchan <sa0466@nate.com>
2018-05-29 11:11:09 +0900

    2018-05-29_3

--- pages/dogs/_dog.vue
+++ pages/dogs/_dog.vue
@@ -1,6 +1,13 @@
 <template>
  <section class="section">
- 
+     <article class="hero"> 
+         <div class="hero-body">
+             <div class="container">
+                 <h1 class="title">저는 {{dogName.toUpperCase()}}입니다.</h1>
+                 <h2 class="subtitle">많이 사랑해주세요!</h2>
+         </div>
+     </article>
+     {{dogname}}
  </section>
 </template>
 <script>
