e9dbd3936285049c17e19ccd158d4fb739cae148
limjuchan <sa0466@nate.com>
2018-05-29 12:24:17 +0900

    2018-05-29_8

--- pages/dogs/_dog.vue
+++ pages/dogs/_dog.vue
@@ -15,22 +15,19 @@
                         <img :src="image" alt="">
                     </figure>
                 </div>
-
             </div>
         </article>
         <article class="message is-danger">
             <div class="message-header">
-                <p>{{dogname.toUpperCase()}}의 아품종</p>
+                <p>{{dogName.toUpperCase()}}의 아품종</p>
             </div>
             <div class="message-body">
                 <ol>
                     <li v-for="subdog in dogList" :key="subdog">
                         {{subdog.toUpperCase()}}
-                    </li>  
+                    </li>
                 </ol>
-                        
             </div>
-
         </article>
     </section>
 </template>
@@ -44,12 +41,12 @@
                 const image = await axios.get(`https://dog.ceo/api/breed/${params.dog}/images/random`);
                 images.push(image.data.message);
             }
-            const list = axios.get('https:dog.ceo/api/breed/${params.dog}/list');
+            const list = await axios.get(`https://dog.ceo/api/breed/${params.dog}/list`);
             return {
                 dogName: params.dog,
                 images: images,
-                dogList : list.data.message
+                dogList: list.data.message
             };
         }
     };
-</script>np
+</script>
