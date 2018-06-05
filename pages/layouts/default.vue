984ba97b73fa54ac34c517345f288f3237499333
limjuchan <sa0466@nate.com>
2018-06-05 10:54:36 +0900

    2018-06-05_4

--- pages/layouts/default.vue
+++ pages/layouts/default.vue
@@ -2,17 +2,26 @@
    <div> 
      <nav class="navbar"> 
        <div class="navbar-brand"> 
-         <nuxt-link to="/" class="navbar-item">개사랑</nuxt-link> 
+         <nuxt-link to="/" class="navbar-item"> 
+           <figure class="image is-64x64"> 
+             <img src="~static/dogPaw.png" alt=""> 
+           </figure> 
+         </nuxt-link> 
        </div> 
        <div class="navbar-menu is-active"> 
-         <div class="navbar-end">            <nuxt-link to="/stat" class="navbar-item">통계</nuxt-link> 
+         <div class="navbar-end"> 
+           <nuxt-link to="/stat" class="navbar-item">통계</nuxt-link> 
            <nuxt-link to="/about" class="navbar-item">소개</nuxt-link> 
          </div> 
        </div> 
      </nav> 
      <nuxt/> 
      <footer class="footer has-text-centered"> 
-       <p>2018년 목원대학교 개사랑. 모든 저작권은 개사랑에 있습니다.</p> 
+       <div> 
+         <figure class="image is-32x32"> 
+           <img src="~assets/dog.png" alt=""> 
+         </figure> 2018년 목원대학교 개사랑. 모든 저작권은 개사랑에 있습니다.</div> 
      </footer> 
    </div> 
  </template> 
+
